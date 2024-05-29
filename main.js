c260a47bbb1b6c52cf11a8dede3630b767973009
Elizabeth Bordjieva <201221014@fdiba.tu-sofia.bg>
2024-05-29 16:15:19 +0300

    Design updated, animations added

--- main.js
+++ main.js
@@ -2,3 +2,24 @@ document.getElementById('rsvp-form').addEventListener('submit', function (event)
     event.preventDefault();
     document.querySelector('.thank-you').style.display = 'block';
 });
+
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('in-view')
+            }
+        });
+    },
+    {
+        rootMargin: '0px',
+        threshold: [0, 0.1, 1],
+    });
+
+const paragraph = document.querySelectorAll('.answer');
+//const button = document.querySelectorAll('button');
+
+paragraph.forEach((element) => {
+    observer.observe(element)
+})
+
